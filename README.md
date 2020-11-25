611a20af6eea37261b906ebd27646180c81e3e25
Neuton Foo <neuton.foo@gmail.com>
2020-11-24 16:56:18 -0800

    Iniitial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Really Simple Spotify Widget
+
+A REALLY simple Now Playing Spotify Übersicht widget.
+
+![Really Simple Spotify Widget](screenshot.png)
+
+![Really Simple Spotify Widget](screenshot2.png)
+
+## Installation
+
+Place the widget into the Übersicht widget directory.
+
+Ensure that you have `npm` or `yarn` installed.
+
+```sh
+# cd into your widget directory
+
+cd Really-Simple-Spotify-Widget
+
+yarn install # or npm install
+```
+
+## Options
+
+```js
+const options = {
+  // top and left position the widget relative the top left corner
+  top: "20px",
+  left: "20px",
+
+  // The maximum width of the widget. Height is variable to the album's height
+  width: "380px",
+
+  // Progress bar's height
+  progressBarHeight: "7px",
+
+  // Font used to display song name and artist
+  fontFamily: "Gill Sans",
+
+  // Song and artist font sizes
+  songFontSize: "22pt",
+  artistFontSize: "14px",
+
+  // If defined, will be used as song and artist font color
+  // Leave commented (null) for average color of album art
+  // accent: "#FFF",
+};
+```
---
+++ index.jsx
@@ -0,0 +1,151 @@
+import { css } from "uebersicht";
+import FastAverageColor from "fast-average-color";
+
+const options = {
+  top: "20px",
+  left: "20px",
+  width: "380px",
+  progressBarHeight: "7px",
+  fontFamily: "Gill Sans",
+  songFontSize: "22pt",
+  artistFontSize: "14px",
+  // accent: "#FFF",
+};
+
+export const command =
+  "osascript ./Really-Simple-Spotify-Widget/now-playing.scpt | echo";
+export const refreshFrequency = 1000;
+
+const fac = new FastAverageColor();
+let album_color = {
+  r: 0,
+  g: 0,
+  b: 0,
+};
+
+export const className = {
+  top: options.top,
+  left: options.left,
+  userSelect: "none",
+};
+
+const containerClassName = css({
+  width: options.width,
+});
+
+const albumCoverClassName = css({
+  width: "100%",
+  display: "block",
+  borderRadius: "5px",
+});
+
+const progressClassName = css({
+  marginTop: "7px",
+  width: "100%",
+  height: options.progressBarHeight,
+  borderRadius: "5px",
+});
+
+const songClassName = css({
+  textAlign: "center",
+  fontSize: options.songFontSize,
+  fontFamily: options.fontFamily,
+});
+
+const artistClassName = css({
+  textAlign: "center",
+  fontFamily: options.fontFamily,
+  fontSize: options.artistFontSize,
+  fontStyle: "italic",
+});
+
+export const updateState = (event, previousState) => {
+  if (event.error) {
+    return { ...previousState, warning: `We got an error: ${event.error}` };
+  }
+
+  const [
+    _,
+    is_playing,
+    song,
+    artist,
+    album,
+    album_art,
+    duration,
+    seconds_played,
+  ] = event.output.split("\n");
+
+  if (is_playing === "false") {
+    return {
+      is_playing: false,
+    };
+  }
+
+  fac.getColorAsync(album_art).then(response => {
+    album_color = {
+      r: response.value[0],
+      g: response.value[1],
+      b: response.value[2],
+    };
+  });
+
+  return {
+    is_playing: true,
+    song: song,
+    artist: artist,
+    album: album,
+    album_art: album_art,
+    duration: duration,
+    seconds_played: seconds_played,
+  };
+};
+
+export const render = ({
+  is_playing,
+  song,
+  artist,
+  album,
+  album_art,
+  duration,
+  seconds_played,
+}) => {
+  if (!is_playing) {
+    return <p className={containerClassName}>Not PLaying</p>;
+  }
+
+  const mainColor = `rgb(${album_color.r}, ${album_color.g}, ${album_color.b})}`;
+
+  return (
+    <div className={containerClassName}>
+      <img
+        id="albumCover"
+        className={albumCoverClassName}
+        src={album_art}
+        draggable="false"
+      />
+      <div className={progressClassName}>
+        <div
+          className={`${progressClassName}
+          ${css({
+            backgroundColor: options.accent ? options.accent : mainColor,
+            width: `${(seconds_played / duration) * 1000 * 100}%`,
+          })}`}
+        ></div>
+      </div>
+      <div
+        className={`${songClassName} ${css({
+          color: options.accent ? options.accent : mainColor,
+        })}`}
+      >
+        {song}
+      </div>
+      <div
+        className={`${artistClassName} ${css({
+          color: options.accent ? options.accent : mainColor,
+        })}`}
+      >
+        {artist}
+      </div>
+    </div>
+  );
+};
